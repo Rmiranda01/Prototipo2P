607e1db1a3670d419cc37bde93c51cb1a19496d2
Rmiranda01 <rmirandav@miumg.edu.gt>
2022-09-27 17:41:51 -0600

    Capa de Vista

---
+++ Prototipo2P/CapaVista/Control de Cheques.cs
@@ -0,0 +1,91 @@
+﻿using CapaControlador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class Control_de_Cheques : Form
+    {
+        Controlador cn = new Controlador();
+        string table = "control_cheques";
+
+        public void llenar()
+        {
+            DataTable dt = cn.llenarTbl(table);
+            Dtg1.DataSource = dt;
+        }
+        public Control_de_Cheques()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnagregar_Click(object sender, EventArgs e)
+        {
+            if (Txtcodigo.Text == "" || Txtconcepto.Text == "" || Txtmonto.Text == "" || Txtcuentabene.Text == "" || Txtfecha.Text == "" || Txtbanco.Text == "" || Txtidbanco.Text == "" || Txtidcuentabe.Text == "")
+            {
+                MessageBox.Show("COMPLETAR INFORMACION");
+            }
+            else
+            {
+
+                TextBox[] textbox = { Txtcodigo, Txtconcepto, Txtmonto, Txtcuentabene, Txtfecha, Txtbanco, Txtidbanco, Txtidcuentabe };
+                cn.ingresar(textbox, table);
+                string message = "Registro Guardado";
+                MessageBox.Show(message);
+                llenar();
+            }
+        }
+
+        private void Btnmostrar_Click(object sender, EventArgs e)
+        {
+            llenar();
+        }
+
+        private void Btnmodificar_Click(object sender, EventArgs e)
+        {
+            if (Txtcodigo.Text == "" || Txtconcepto.Text == "" || Txtmonto.Text == "" || Txtcuentabene.Text == "" || Txtfecha.Text == "" || Txtbanco.Text == "" || Txtidbanco.Text == "" || Txtidcuentabe.Text == "")
+            {
+                MessageBox.Show("COMPLETAR INFORMACION");
+
+            }
+            else
+            {
+                TextBox[] textbox = { Txtcodigo, Txtconcepto, Txtmonto, Txtcuentabene, Txtfecha, Txtbanco, Txtidbanco, Txtidcuentabe };
+                int valor1 = int.Parse(Txtcodigo.Text);
+                string campo = "idControl_Cheques = ";
+                cn.actualizar(textbox, table, campo, valor1);
+                MessageBox.Show("Dato modificado");
+                llenar();
+            }
+        }
+
+        private void Btneliminar_Click(object sender, EventArgs e)
+        {
+            string message = "Deseas Eliminar el Registro?";
+            string title = "Eliminar Registro";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                int campo = int.Parse(Txtcodigo.Text);
+                string condicion = "idControl_Cheques = ";
+                cn.eliminar(table, condicion, campo);
+                llenar();
+                MessageBox.Show("Registro Eliminado");
+
+            }
+            else
+            {
+                llenar();
+
+            }
+        }
+    }
+}
---
+++ Prototipo2P/CapaVista/Menu.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace CapaVista
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.catalogosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bancosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controlChequesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.catalogosToolStripMenuItem,
+            this.procesosToolStripMenuItem,
+            this.informesToolStripMenuItem,
+            this.herramientasToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // catalogosToolStripMenuItem
+            // 
+            this.catalogosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bancosToolStripMenuItem});
+            this.catalogosToolStripMenuItem.Name = "catalogosToolStripMenuItem";
+            this.catalogosToolStripMenuItem.Size = new System.Drawing.Size(90, 24);
+            this.catalogosToolStripMenuItem.Text = "Catalogos";
+            // 
+            // procesosToolStripMenuItem
+            // 
+            this.procesosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.controlChequesToolStripMenuItem});
+            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.procesosToolStripMenuItem.Text = "Procesos";
+            // 
+            // informesToolStripMenuItem
+            // 
+            this.informesToolStripMenuItem.Name = "informesToolStripMenuItem";
+            this.informesToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.informesToolStripMenuItem.Text = "Informes";
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // bancosToolStripMenuItem
+            // 
+            this.bancosToolStripMenuItem.Name = "bancosToolStripMenuItem";
+            this.bancosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.bancosToolStripMenuItem.Text = "Bancos";
+            this.bancosToolStripMenuItem.Click += new System.EventHandler(this.bancosToolStripMenuItem_Click);
+            // 
+            // controlChequesToolStripMenuItem
+            // 
+            this.controlChequesToolStripMenuItem.Name = "controlChequesToolStripMenuItem";
+            this.controlChequesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.controlChequesToolStripMenuItem.Text = "Control Cheques";
+            this.controlChequesToolStripMenuItem.Click += new System.EventHandler(this.controlChequesToolStripMenuItem_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Menu";
+            this.Text = "Ricardo_Miranda_0901-09-3269";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem catalogosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procesosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bancosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controlChequesToolStripMenuItem;
+    }
+}
